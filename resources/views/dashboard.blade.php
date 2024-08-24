a59c2eb678ea8bf500f997a64b45f98f7f131b59
unknown <kamalgandonou@gmail.com>
2024-08-24 19:45:16 +0200

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,2 +1,5 @@
 <a id="logout" href="login.html"
 class="nav-link">Deconnecter <i class="icon-logout"></i></a>
+
+<a id="logout" href="login.html"
+class="nav-link">connecter <i class="icon-logout"></i></a>
